800477a8d75c5bcae6f4cf9fa7a99cfa4fd5705d
Jacob Logas <logasja@outlook.com>
2024-05-26 19:28:24 -0400

    HuggingFace

--- fdf_dataset_builder.py
+++ fdf_dataset_builder.py
@@ -20,27 +20,78 @@ import urllib
 
 import numpy as np
 
-from tensorflow_datasets.core.utils.lazy_imports_utils import tensorflow as tf
-import tensorflow_datasets.public_api as tfds
+import datasets
+
+_CITATION = """\
+@InProceedings{10.1007/978-3-030-33720-9_44,
+author="Hukkel{\aa}s, H{\aa}kon
+and Mester, Rudolf
+and Lindseth, Frank",
+title="DeepPrivacy: A Generative Adversarial Network for Face Anonymization",
+booktitle="Advances in Visual Computing",
+year="2019",
+publisher="Springer International Publishing",
+pages="565--578",
+isbn="978-3-030-33720-9"}
+"""
+
+_DESCRIPTION = """\
+Flickr Diverse Faces (FDF) is a dataset with 1.5M faces "in the wild".
+FDF has a large diversity in terms of facial pose, age, ethnicity, occluding objects, facial painting, and image background.
+The dataset is designed for generative models for face anonymization, and it was released with the paper DeepPrivacy: A Generative Adversarial Network for Face Anonymization.
+
+The dataset was crawled from the website Flickr (YFCC-100M dataset) and automatically annotated.
+Each face is annotated with 7 facial landmarks (left/right ear, lef/right eye, left/right shoulder, and nose), and a bounding box of the face.
+Our paper goes into more detail about the automatic annotation.
+"""
 
 _URL = "https://api.loke.aws.unit.no/dlr-gui-backend-resources-content/v2/contents/links/"
 RESOURCES = {
-  "metadata": tfds.download.Resource(url=urllib.parse.urljoin(_URL, "b704049a-d465-4a07-9cb3-ca270ffab80292e4d5ac-6172-4d37-bf63-4438f61f8aa0e1f6483d-5d45-40b5-b356-10b71fc00e89")),
-  "cc-by-nc-sa-2": tfds.download.Resource(url=urllib.parse.urljoin(_URL, "33bb6132-a30e-4169-a09e-48b94cd5e09010fd8f59-1db9-4192-96f0-83d18adf50b4ee3ab25c-b8f8-4c40-a8ba-eaa64d830412")),
-  "cc-by-2": tfds.download.Resource(url=urllib.parse.urljoin(_URL, "cb545564-120f-4f35-8b68-63e59e4fd273b1c36452-21e7-4976-85dd-a86c0738ebc256264f20-f969-4a82-ae1c-f6345d8e8d1f")),
-  "cc-by-sa-2": tfds.download.Resource(url=urllib.parse.urljoin(_URL, "4e5c27bd-f5fd-4dd3-bf2b-4434a8952df0ff4d11f8-e993-4517-9378-b35d89e7882ecae76ce7-88a0-417b-b5d8-e030863e97f6")),
-  "cc-by-nc-2": tfds.download.Resource(url=urllib.parse.urljoin(_URL, "da46d666-4378-4e75-9182-e683ebe08f2e9203750f-7ed8-42f7-8bce-90a7dfddd3764401df36-a3c3-4d39-ae8e-0cb4397e5c74")),
+  "metadata": urllib.parse.urljoin(_URL, "b704049a-d465-4a07-9cb3-ca270ffab80292e4d5ac-6172-4d37-bf63-4438f61f8aa0e1f6483d-5d45-40b5-b356-10b71fc00e89"),
+  "cc-by-nc-sa-2": urllib.parse.urljoin(_URL, "33bb6132-a30e-4169-a09e-48b94cd5e09010fd8f59-1db9-4192-96f0-83d18adf50b4ee3ab25c-b8f8-4c40-a8ba-eaa64d830412"),
+  "cc-by-2": urllib.parse.urljoin(_URL, "cb545564-120f-4f35-8b68-63e59e4fd273b1c36452-21e7-4976-85dd-a86c0738ebc256264f20-f969-4a82-ae1c-f6345d8e8d1f"),
+  "cc-by-sa-2": urllib.parse.urljoin(_URL, "4e5c27bd-f5fd-4dd3-bf2b-4434a8952df0ff4d11f8-e993-4517-9378-b35d89e7882ecae76ce7-88a0-417b-b5d8-e030863e97f6"),
+  "cc-by-nc-2": urllib.parse.urljoin(_URL, "da46d666-4378-4e75-9182-e683ebe08f2e9203750f-7ed8-42f7-8bce-90a7dfddd3764401df36-a3c3-4d39-ae8e-0cb4397e5c74"),
 }
 
-FDF_IMAGE_SHAPE = (256, 256, 3)
-
-
-class Builder(tfds.core.GeneratorBasedBuilder):
+class Builder(datasets.GeneratorBasedBuilder):
   """FDF Builder class."""
 
-  VERSION = tfds.core.Version("0.1.0")
+  VERSION = datasets.Version("0.1.0")
+
+  BUILDER_CONFIGS = [
+    datasets.BuilderConfig(name="default", version=VERSION, description="Default data of the FDF dataset."),
+    datasets.BuilderConfig(name="images", version=VERSION, description="FDF dataset with just the images and no boundign boxes.")
+  ]
+
+  DEFAULT_CONFIG_NAME = "default"
 
   def _info(self):
+    if self.config.name == "default":
+      features = datasets.features.Features(
+        {
+          "image": datasets.features.Image(),
+          "landmarks": {
+            'nose': datasets.features.Array2D(shape=(2,)),
+            'r_eye': datasets.features.Array2D(shape=(2,)),
+            'l_eye': datasets.features.Array2D(shape=(2,)),
+            'r_ear': datasets.features.Array2D(shape=(2,)),
+            'l_ear': datasets.features.Array2D(shape=(2,)),
+            'r_shoulder': datasets.features.Array2D(shape=(2,)),
+            'l_shoulder': datasets.features.Array2D(shape=(2,)),
+          },
+          "bbox": datasets.features.Sequence(feature=datasets.features.Value('float32'), length=4),
+        }
+      )
+    elif self.config.name == "images":
+      features = datasets.features.Features(
+        {
+          "image": datasets.features.Image(),
+        }
+      )
+    else:
+      raise Exception("Config not recognized")
+
     return self.dataset_info_from_configs(
         features=tfds.features.FeaturesDict({
           "image": tfds.features.Image(shape=FDF_IMAGE_SHAPE),
